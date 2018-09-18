9d1d6d370586eec007680fb4e1aac7bca836d22f
rowan gates <rowangates@hotmail.co.uk>
2018-09-18 12:32:30 +0100

    Added initial data models

---
+++ ComicStandingOrderManager/ComicStandingOrderManager/DataModels/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicStandingOrderManager.DataModels
+{
+    internal class Customer : ICustomer
+    {
+        private int Id;
+        private string FirstName;
+        private string LastName;
+        private string Email;
+        private IList<IComicSeries> StandingOrders;
+    }
+}
