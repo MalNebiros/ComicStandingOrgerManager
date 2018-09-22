c168c9dbeb76539952cd96bde91162ad066bafce
rowan gates <rowangates@hotmail.co.uk>
2018-09-22 18:16:13 +0100

    Customer Entry classes

---
+++ ComicStandingOrderManager/ComicStandingOrderManager/ViewModels/CustomersEntryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicStandingOrderManager.ViewModels
+{
+    internal class CustomersEntryViewModel
+    {
+        public int customerId;
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string emailAddress { get; set; }
+        public int numberOfSeriesSubscribedTo { get; set; }
+    }
+}
---
+++ ComicStandingOrderManager/ComicStandingOrderManager/Views/CustomersEntryView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ComicStandingOrderManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComicStandingOrderManager.Views
+{
+    /// <summary>
+    /// Interaction logic for CustomersEntryView.xaml
+    /// </summary>
+    public partial class CustomersEntryView : Page
+    {
+        CustomersEntryViewModel _customerEntryViewModel;
+        public CustomersEntryView()
+        {
+            InitializeComponent();
+            _customerEntryViewModel = new CustomersEntryViewModel();
+            DataContext = _customerEntryViewModel;
+        }
+    }
+}
